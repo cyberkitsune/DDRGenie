05abe24a19a032b174ee253e0db63f24d240d5d2
CyberKitsune <cyberkitsune09@gmail.com>
2020-02-17 13:12:23 -0800

    Clean up test script

--- DDRGenie.py
+++ DDRGenie.py
@@ -1,11 +1,13 @@
 from PIL import Image
 from DDRDataTypes import DDRScreenshot, DDRParsedData
-import sys, PIL
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv < 2):
+        print("Usage: DDRGenie.py [path to screenshot image file]")
+        exit(0)
     sshot = Image.open(sys.argv[1])
     i = DDRScreenshot(sshot)
-    #i.play_money_score.show()
     d = DDRParsedData(i)
     print(d)
 
